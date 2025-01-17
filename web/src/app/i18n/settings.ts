ff7f343d20876dff62ec9f8127db131fb19c8fe7
lavren1974 <shadowchess.org@gmail.com>
2025-01-17 09:59:40 +0300

    добавлены переводы на французский, русский и немецкий язык

--- web/src/app/i18n/settings.ts
+++ web/src/app/i18n/settings.ts
@@ -1,5 +1,6 @@
+// app/i18n/settings.ts
 export const fallbackLng = 'en'
-export const languages = [fallbackLng, 'es']
+export const languages = [fallbackLng, 'es', 'fr', 'de', 'ru']
 export const defaultNS = 'common'
 
 export function getOptions(lng = fallbackLng, ns = defaultNS) {
